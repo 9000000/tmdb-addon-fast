8e4d95040d2c31213fa843b262ecb6612601f6ce
9000000 <hai90000001@gmail.com>
2026-05-25 21:20:28 +0700

    up

--- addon/utils/httpClient.js
+++ addon/utils/httpClient.js
@@ -28,7 +28,7 @@ const TMDB_DOMAINS = [
  */
 function shouldUseProxy(url) {
   if (!PROXY_CONFIG.enabled) return false;
-  
+
   try {
     const urlObj = new URL(url);
     return TMDB_DOMAINS.some(domain => urlObj.hostname.includes(domain));
@@ -47,13 +47,13 @@ function createAxiosInstance(url) {
   const config = {
     timeout: 30000,
     headers: {
-      'User-Agent': 'TMDB-Addon/5.0.1'
+      'User-Agent': 'TMDB-Addon/5.0.2'
     }
   };
 
   if (shouldUseProxy(url)) {
     console.log(`Using proxy for: ${url}`);
-    
+
     const proxyConfig = {
       host: PROXY_CONFIG.host,
       port: PROXY_CONFIG.port,
@@ -65,7 +65,7 @@ function createAxiosInstance(url) {
     }
 
     config.proxy = proxyConfig;
-    
+
     // Additional configuration for HTTPS through proxy
     if (PROXY_CONFIG.protocol === 'https') {
       config.httpsAgent = new https.Agent({
