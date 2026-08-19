2e277a2780a952c27546a18ce54de10d47be8301
9000000 <hai90000001@gmail.com>
2026-08-19 15:57:02 +0700

    têt

--- addon/index.js
+++ addon/index.js
@@ -8,7 +8,7 @@ const { getSearch } = require("./lib/getSearch");
 const { getManifest, DEFAULT_LANGUAGE } = require("./lib/getManifest");
 const { getMeta } = require("./lib/getMeta");
 const { getTmdb } = require("./lib/getTmdb");
-const { cacheWrapMeta } = require("./lib/getCache");
+
 const { getTrending } = require("./lib/getTrending");
 const { parseConfig, getRpdbPoster } = require("./utils/parseProps");
 const { getRequestToken, getSessionId } = require("./lib/getSession");
@@ -362,9 +362,7 @@ const handleMeta = async function (req, res) {
     try {
       const tmdbId = await getTmdb(type, imdbId, config);
       if (tmdbId) {
-        const resp = await cacheWrapMeta(`${language}:${type}:${tmdbId}`, async () => {
-          return await getMeta(type, language, tmdbId, config);
-        });
+        const resp = await getMeta(type, language, tmdbId, config);
         const cacheOpts = {
           staleRevalidate: 20 * 24 * 60 * 60,
           staleError: 30 * 24 * 60 * 60,
